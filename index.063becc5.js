796c2f9ac34f6c8781b5b88a4e41f356c9329746
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-09-02 19:41:32 +0000

    Deploying to gh-pages from @ Zhihare/BookShop@ae29be4095efe9d06661ea5e2f3d2d5509b84b26 🚀

---
+++ index.063becc5.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".toggle"),t=document.querySelector("body");e.addEventListener("click",(function(){e.classList.toggle("active"),t.classList.toggle("dark-body-theme")}));
+//# sourceMappingURL=index.063becc5.js.map
---
+++ index.f4e35400.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".toggle"),t=document.querySelector("body");e.addEventListener("click",(function(){e.classList.toggle("active"),t.classList.toggle("dark-body-theme")}))}();
+//# sourceMappingURL=index.f4e35400.js.map
